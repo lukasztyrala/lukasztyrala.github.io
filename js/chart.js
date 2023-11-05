6323d87bb0ea9a89c894b29c7ade0c2a53c327b4
lukasz <l.tyrala@gmail.com>
2023-11-05 16:14:06 +0100

    Added uplot for some stats in now.html

---
+++ js/chart.js
@@ -0,0 +1,78 @@
+// Prepare the data
+const currentYear = new Date().getFullYear();
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const distancesCurrentYear =  [ 71, 141, 279, 318,  85, 365, 472, 497, 302, 276, 224, 157];
+const distancesPreviousYear = [ 97, 100,  82, 275, 293, 270, 197, 272,  83, 255,  89,   0];
+
+// Data structure for uPlot
+const data = [
+  months.map((m, i) => i), // Convert month names to indices
+  distancesPreviousYear,
+  distancesCurrentYear,
+];
+
+// Set the options
+const options = {
+  height: 200,
+  series: [
+    { value: (self, rawValue) => months[rawValue] }, // Assuming months are 0-indexed
+    {
+      label: "2022",
+      stroke: 'gray',
+      width: 2,
+      value: (self, rawValue) => rawValue + ' km', // Append 'km' units to series values
+    },
+    {
+      label: "2023",
+      stroke: 'blue',
+      width: 2,
+      value: (self, rawValue) => rawValue + ' km', // Append 'km' units to series values
+    },
+  ],
+ axes: [
+   {
+     values: (self, ticks) => ticks.map(idx => months[idx]), // Format x-axis labels
+     grid: { show: false }, // Hide x-axis gridlines
+   },
+   {
+     show: false, // Hide y-axis labels and gridlines
+     grid: { show: false },
+   },
+ ],
+};
+
+// Target a DOM element
+const element = document.getElementById('chart');
+
+// 
+
+function createChart() {
+  const element = document.getElementById('chart');
+  if (element) {
+    element.innerHTML = '';
+
+    // Since the DOM is fully loaded, no need for requestAnimationFrame
+    const uplot = new uPlot(options, data, element);
+
+    // Set the initial size explicitly
+    uplot.setSize({
+      width: element.clientWidth,
+      height: options.height
+    });
+
+    // Resize handling
+    let resizeTimer;
+    window.addEventListener('resize', () => {
+      clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(() => {
+        uplot.setSize({
+          width: element.clientWidth,
+          height: options.height
+        });
+      }, 100);
+    });
+  }
+}
+
+// Execute immediately since this script is loaded at the end of the body
+createChart();
