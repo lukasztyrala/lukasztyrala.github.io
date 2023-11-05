9fd1258fa9c73adff977f288dd65f8e559150ead
lukasz <l.tyrala@gmail.com>
2023-11-05 22:41:15 +0100

    Bike chart updates

--- js/chart.js
+++ js/chart.js
@@ -1,14 +1,14 @@
 // Prepare the data
 const currentYear = new Date().getFullYear();
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const distancesCurrentYear =  [ 71, 141, 279, 318,  85, 365, 472, 497, 302, 276, 224, 157];
-const distancesPreviousYear = [ 97, 100,  82, 275, 293, 270, 197, 272,  83, 255,  89,   0];
+const distances2022 =  [ 71, 141, 279, 318,  85, 365, 472, 497, 302, 276, 224, 157];
+const distances2023 = [ 97, 100,  82, 275, 293, 270, 197, 272,  83, 255,  89, NaN];
 
 // Data structure for uPlot
 const data = [
   months.map((m, i) => i), // Convert month names to indices
-  distancesPreviousYear,
-  distancesCurrentYear,
+  distances2022,
+  distances2023,
 ];
 
 // Set the options
@@ -18,13 +18,13 @@ const options = {
     { value: (self, rawValue) => months[rawValue] }, // Assuming months are 0-indexed
     {
       label: "2022",
-      stroke: 'gray',
+      stroke: '#a1c0cc',
       width: 2,
       value: (self, rawValue) => rawValue + ' km', // Append 'km' units to series values
     },
     {
       label: "2023",
-      stroke: 'blue',
+      stroke: '#223e5d',
       width: 2,
       value: (self, rawValue) => rawValue + ' km', // Append 'km' units to series values
     },
