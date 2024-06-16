5a9fe791694083912a4b7fcc8ff8517872210112
lukasz <l.tyrala@gmail.com>
2024-06-16 21:09:14 +0200

    Initial index.html and partials

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Łukasz Tyrała &ndash; professional design services &amp; education since 2008';
+export const SITE_DESCRIPTION = 'Kraków-based designer, educator and team leader focused on holistic user experience (ux reserach, interaction and interface design) and digital products management';
